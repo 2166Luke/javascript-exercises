9b38166875f187e34679a105cb94e0bb48b78731
2166Luke <lukehancock2166@gmail.com>
2022-03-21 19:56:45 -0400

    Completed Exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    const result = string;
+    for (i = 0; i < num; i++){
+        return result.repeat(num);
+    }
+    if(num == 0){return ""}
+    else if(num < 0) {return 'ERROR'}
+    else if(string[0] == "") {return ""}
 };
 
+const number = Math.floor(Math.random() * 1000)
+
+repeatString('hey', 3)
+repeatString('hey', 10)
+repeatString('hey', 1)
+repeatString('hey', 0)
+repeatString('hey', -1)
+repeatString('hey', number)
+repeatString('',10)
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === theArgs.reduce()) {
+            array.splice(i,1);
+            i--;
+        }
+    }
+    return array;
 };
+
+removeFromArray([1,2,3,4],3);
+removeFromArray([1,2,3,4],3,2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
