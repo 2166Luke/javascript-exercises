6c57e0f78d29f02948b18e43c7505c876df95e67
Claire Campbell <clairecampbell@Claires-MacBook-Air-2.local>
2024-06-12 11:27:38 -0500

    worked on calculator js exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let difference = a - b;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  let product = a * b;
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	power = Math.pow(a, b);
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let factorial = 1;
+	for (let i = n; i >1; i--) {
+    factorial *= i;
+}
+return factorial;
 };
 
 // Do not edit below this line
